5fcedf689f693c899449a456fef9d24820e121a6
Jonathan Turner <turner@atlantissrv.com>
2024-05-01 14:41:25 -0400

    Modified ServicesRegistry.py to register/unregister services.

--- src/ServicesRegistry.py
+++ src/ServicesRegistry.py
@@ -1,24 +1,37 @@
 from zeroconf import ServiceListener, Zeroconf, ServiceInfo
 import socket
 
+info = None
+
+
 class ServicesRegistry(ServiceListener):
+    info = None
 
-    def update_service(self, zc: 'Zeroconf', type_: str, name: str) -> None:
-        info = zc.get_service_info(type_, name)
-        if info:
-            print("Service %s updated, service info: %s" % (name, info))
+    def unregister(self, zc: Zeroconf) -> None:
+        global info
+        if info is not None:
+            zc.unregister_service(info)
+            print(f"Service removed")
+        else:
+            print(f"Service not found")
 
-    def remove_service(self, zc: Zeroconf, type_: str, name: str) -> None:
-        print(f"Service {name} removed")
+    def register(self, zc: Zeroconf, name: str, desc: str, props: dict, port: int) -> None:
+        global info
 
-    def register(self, zc: Zeroconf, name: str) -> None:
+        local_ip = socket.gethostbyname(socket.gethostname())
+        address = socket.inet_aton(local_ip)
+        fName = '_' + name.lower() + '._tcp.local.'
+        fDesc = desc + '.' + fName
+
         info = ServiceInfo(
-            "_styxmix._tcp.local.",
-            "StyxMix Audio Control._styxmix._tcp.local.",
-            addresses=[socket.inet_aton("127.0.0.1")],
-            port=18200,
-            properties={'version': '0.0', 'a': 'test value', 'b': 'another value'},
+            fName,
+            fDesc,
+            addresses=[address],
+            port=port,
+            properties=props,
         )
 
         Zeroconf().register_service(info)
-        print('Service registered')
+        print(
+            f'Service registered under name: _{name.lower()}.tcp.local., description: {desc}._{name.lower()}._tcp.local., port: {port}'
+            f' and properties: {props}')
