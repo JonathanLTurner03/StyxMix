c8a3ebf045d2ad3aeb1943d742beecc8090c548c
Jonathan Turner <turner@atlantissrv.com>
2024-05-04 00:45:43 -0400

    Client and Server now can connect without the need of specifying ports.

--- src/helper/browser.py
+++ src/helper/browser.py
@@ -4,6 +4,9 @@ from zeroconf import ServiceBrowser, Zeroconf
 import ServicesRegistry as Registry
 import socket
 
+from zeroconf import ZeroconfServiceTypes
+print('\n'.join(ZeroconfServiceTypes.find()))
+
 zeroconf = Zeroconf()
 listener = Registry.ServicesMonitor()
 browser = ServiceBrowser(zeroconf, "_styxmix._tcp.local.", listener)
--- src/server.py
+++ src/server.py
@@ -1,8 +1,13 @@
 # Port used 18200
 from zeroconf import Zeroconf, ServiceBrowser
 import src.helper.ServicesRegistry as Registry
+
+import zmq
+import socket
+
 import yaml
 import os
+import time
 
 config = {}
 
@@ -14,16 +19,24 @@ zeroconf = Zeroconf()
 listener = Registry.ServicesMonitor()
 browser = ServiceBrowser(zeroconf, "_styxmix._tcp.local.", listener)
 
+context = zmq.Context()
+zmqComms = context.socket(zmq.REQ)
 
-try:
-    input("Press enter to exit...\n\n")
-finally:
-    pass
+name = '_' + config['name'] + '._tcp.local.'
+desc = config['zeroconf']['desc'] + '.' + name
 
-Registry.unregister(zeroconf)
+while True:
+    services = zeroconf.get_service_info(name, desc)
+    if services:
+        ip_address = socket.inet_ntoa(services.addresses[0])
+        port = services.port
+        zmqComms.connect(f"tcp://{ip_address}:{port}")
 
-try:
-    input("Press enter to continue")
-finally:
-    zeroconf.close()
-    exit(0)
+        zmqComms.send_string("StyxMix Handshake: Host IP," + Registry.get_local_ipv4())
+        message = zmqComms.recv_string()
+        if "StyxMix Handshake: Successful." in message:
+            print("Connected to client.")
+            break
+    else:
+        print("No client found. Retrying in 5 seconds...")
+        time.sleep(5)
